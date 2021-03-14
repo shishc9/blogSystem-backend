fe18fec32f929a14101b075c3ac523cd6c2c37b3
shishc9 <343141457@qq.com>
2021-03-14 21:48:37 +0800

    feat : add SwaggerConfig and banner.txt
    可以通过 localhost:9090/swagger-ui.html 进行接口测试

--- src/main/java/icu/shishc/BlogApplication.java
+++ src/main/java/icu/shishc/BlogApplication.java
@@ -2,6 +2,8 @@ package icu.shishc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @PackageName:icu.shishc
@@ -10,8 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@RestController
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String foo() {
+        return "hello";
     }
 }
