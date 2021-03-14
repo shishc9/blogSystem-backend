c7aff73a13326e6acc2461ebeceb24a5959792ce
shishc9 <343141457@qq.com>
2021-03-14 10:37:56 +0800

    feat : [add dependencies and Application]

---
+++ src/main/java/icu/shishc/BlogApplication.java
@@ -0,0 +1,17 @@
+package icu.shishc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @PackageName:icu.shishc
+ * @Date:2021/3/14, 10:36
+ * @Auther:ShiShc
+ */
+
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
