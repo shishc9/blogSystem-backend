a615a590e565e56202589945d28670caffbacd5b
shishc9 <343141457@qq.com>
2021-04-11 19:21:08 +0800

    feat : [add FastJson and const class]

---
+++ src/main/java/icu/shishc/util/Constants.java
@@ -0,0 +1,12 @@
+package icu.shishc.util;
+
+/**
+ * @date:2021-4-11, 19:11
+ * @author:ShiShc
+ */
+
+// 通用常量类
+public class Constants {
+    public static final String SUCCESS_CODE = "100";
+    public static final String SUCCESS_MSG = "Request succeed";
+}
