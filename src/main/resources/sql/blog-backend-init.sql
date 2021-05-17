fe31db6320943cd8a33704651c69e074b2785222
shishc9 <343141457@qq.com>
2021-05-17 14:30:42 +0800

    fix : [add and update]
    add like table & update blog/user/comment table;

--- src/main/resources/sql/blog-backend-init.sql
+++ src/main/resources/sql/blog-backend-init.sql
@@ -1,41 +1,58 @@
 use blogsys;
 
-INSERT INTO user(username, password, user_identity, age, gender, hobby, email)
-    VALUES('admin', '123456', 1, 20, 'MALE', 'coding', '123456789@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('A', '111111', 20, 'noHobby', '111111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('B', '111111', 20, 'noHobby', '121111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('C', '111111', 20, 'noHobby', '131111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('D', '111111', 20, 'noHobby', '141111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('E', '111111', 20, 'noHobby', '151111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('F', '111111', 20, 'noHobby', '161111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('G', '111111', 20, 'noHobby', '171111111@126.com');
-INSERT INTO user(username, password, age, hobby, email)
-    VALUES('H', '111111', 20, 'noHobby', '181111111@126.com');
+INSERT INTO user(username, password, user_identity, age, gender, email)
+    VALUES('admin', '123456', 1, 20, 'MALE',  '123456789@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('A', '111111', 20, '111111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('B', '111111', 20, '121111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('C', '111111', 20, '131111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('D', '111111', 20, '141111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('E', '111111', 20, '151111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('F', '111111', 20, '161111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('G', '111111', 20, '171111111@126.com');
+INSERT INTO user(username, password, age, email)
+    VALUES('H', '111111', 20, '181111111@126.com');
+INSERT INTO user(username, password, user_identity, age, gender, email)
+    VALUES('admin2', '123456', 1, 20, 'MALE',  '123456780@126.com');
 
 
-INSERT INTO blog(title, content) values ('第一篇博客', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('Java基础', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('Java进阶', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('Java高级', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('JVM', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('高并发', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('分布式', '应该能正常发出去吧');
-INSERT INTO blog(title, content) values ('test', '应该能正常发出去吧');
+
+INSERT INTO blog(user_id, title, content) values (1, '第一篇博客', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, 'Java基础', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, 'Java进阶', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, 'Java高级', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, 'JVM', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, '高并发', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, '分布式', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (1, 'test', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (10, 'admin21', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (10, 'admin22', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (10, 'admin23', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (10, 'admin24', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (10, 'admin25', '应该能正常发出去吧');
+INSERT INTO blog(user_id, title, content) values (10, 'admin26', '应该能正常发出去吧');
 
 
-INSERT INTO comment(username, email, content, gmt_create, parent_comment_id)
-    VALUES('admin', '123456789@126.com', 'first comment', NOW(), 0);
-INSERT INTO comment(username, email, content, gmt_create, parent_comment_id)
-    VALUES('A','111111111@126.com', 'second comment', NOW(), 0);
-INSERT INTO comment(username, email, content, gmt_create, parent_comment_id)
-    VALUES('B','121111111@126.com', 'reply A', NOW(), 5001);
-INSERT INTO comment(username, email, content, gmt_create, parent_comment_id)
-    VALUES('A','111111111@126.com', 'reply B', NOW(), 5002);
+
+INSERT INTO comment(blog_id, username, content, parent_comment_id)
+    VALUES('30000', 'admin', 'first comment', 0);
+INSERT INTO comment(blog_id, username, content, parent_comment_id)
+    VALUES('30000', 'admin', 'second comment', 0);
+
+INSERT INTO comment(blog_id, username, content, parent_comment_id)
+    VALUES('30000', 'A', 'reply admin', 50001);
+
+INSERT INTO comment(username, content, gmt_create, parent_comment_id)
+    VALUES('B', 'leave message1', NOW(), 0);
+INSERT INTO comment(username, content, gmt_create, parent_comment_id)
+    VALUES('A', 'leave message2', NOW(), 0);
+INSERT INTO comment(username, content, gmt_create, parent_comment_id)
+    VALUES('C', 'reply B leave message', NOW(), 50002);
+
 
--- src/main/resources/sql/blog-backend.sql
+++ src/main/resources/sql/blog-backend.sql
@@ -10,24 +10,21 @@ CREATE TABLE user(
                      user_identity TINYINT UNSIGNED DEFAULT 0 COMMENT '是不是博主',
                      age INT(2) UNSIGNED DEFAULT NULL COMMENT '年龄',
                      gender CHAR(6) NOT NULL NULL DEFAULT 'MALE' COMMENT '性别，默认男性',
-                     hobby TEXT COMMENT '爱好',
                      email VARCHAR(64) DEFAULT NULL COMMENT '邮箱',
                      gmt_create DATETIME(0) DEFAULT CURRENT_TIMESTAMP COMMENT '用户创建时间',
                      gmt_last_login DATETIME(0) DEFAULT CURRENT_TIMESTAMP COMMENT '用户上次登陆时间',
                      PRIMARY KEY (user_id) USING BTREE,
                      CONSTRAINT ck CHECK (gender = 'MALE' or gender = 'FEMALE')
-)ENGINE = InnoDB CHARACTER SET = utf8mb4;
-
+)ENGINE = InnoDB CHARACTER SET = utf8mb4 AUTO_INCREMENT = 1;
 # 保证数据库中用户名唯一
-# 一个邮箱只能绑定一个用户 虽然邮箱好像没啥用(目前来看)
+# 一个邮箱只能绑定一个用户
 ALTER TABLE user ADD UNIQUE KEY (username);
 ALTER TABLE user ADD UNIQUE KEY (email);
-
 
 DROP TABLE IF EXISTS blog;
 CREATE TABLE blog(
                      blog_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-                     username VARCHAR(64) DEFAULT 'admin' COMMENT '发布博客的用户，目前只能是admin',
+                     user_id BIGINT UNSIGNED NOT NULL COMMENT '博客所属用户id',
                      title VARCHAR(255) NOT NULL COMMENT '标题',
                      content LONGTEXT COMMENT '正文',
                      status  TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '博客状态，只有0/1，0：公开，1：私有',
@@ -35,12 +32,11 @@ CREATE TABLE blog(
                      like_num INT UNSIGNED DEFAULT 0 COMMENT '博客点赞数',
                      gmt_create DATETIME(0) DEFAULT CURRENT_TIMESTAMP COMMENT '博客创建时间',
                      gmt_modified DATETIME(0) DEFAULT CURRENT_TIMESTAMP COMMENT '博客修改时间',
-                     PRIMARY KEY (blog_id) USING BTREE
-)ENGINE = InnoDB CHARACTER SET = utf8mb4 AUTO_INCREMENT = 1000;
-
+                     PRIMARY KEY (blog_id) USING BTREE,
+                     FOREIGN KEY (user_id) REFERENCES user (user_id)
+)ENGINE = InnoDB CHARACTER SET = utf8mb4 AUTO_INCREMENT = 30000;
 # 保证每个用户不能有两篇及以上相同标题的博客
-# 其实目前规划能写博客的只有admin
-ALTER TABLE blog ADD UNIQUE KEY (username, title);
+ALTER TABLE blog ADD UNIQUE KEY (title);
 
 
 DROP TABLE IF EXISTS comment;
@@ -48,12 +44,23 @@ CREATE TABLE comment (
     comment_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '评论自增id',
     blog_id BIGINT UNSIGNED DEFAULT 0 COMMENT '所属博客id',
     username VARCHAR(64) NOT NULL COMMENT '评论者用户名',
-    email VARCHAR(64) DEFAULT NULL COMMENT '评论者邮箱',
     content VARCHAR(255) NOT NULL COMMENT '评论内容',
     gmt_create DATETIME(0) DEFAULT CURRENT_TIMESTAMP COMMENT '评论发布时间',
     parent_comment_id BIGINT UNSIGNED DEFAULT 0 COMMENT '父评论id',
     PRIMARY KEY (comment_id) USING BTREE,
     FOREIGN KEY (username) REFERENCES user(username)
-)ENGINE = InnoDB CHARACTER SET = utf8mb4 AUTO_INCREMENT = 5000;
+)ENGINE = InnoDB CHARACTER SET = utf8mb4 AUTO_INCREMENT = 50000;
+
+
+
+DROP TABLE IF EXISTS blike;
+CREATE TABLE blike (
+    blog_id BIGINT UNSIGNED NOT NULL COMMENT '点赞的博客id',
+    user_id BIGINT UNSIGNED NOT NULL COMMENT '点赞的用户id',
+    FOREIGN KEY (blog_id) REFERENCES blog (blog_id),
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
+)ENGINE = InnoDB CHARACTER SET = utf8mb4;
+# 同一篇博客一个人只能评论一次
+ALTER TABLE blike ADD UNIQUE KEY (blog_id, user_id);
 
 
