ae1c00c25cca079a3b60d24baaac8ff755650133
shishc9 <343141457@qq.com>
2021-05-19 09:29:37 +0800

    fix : [add LoginFilter]

--- src/main/java/icu/shishc/filter/LoginFilter.java
+++ src/main/java/icu/shishc/filter/LoginFilter.java
@@ -1,14 +1,16 @@
 package icu.shishc.filter;
 
 import com.alibaba.fastjson.JSONObject;
+import icu.shishc.dto.MyDTO;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 /**
  * @date: 2021-5-15, 21:44
@@ -17,122 +19,43 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class LoginFilter extends FormAuthenticationFilter {
-//    @Override
-//    protected boolean isAccessAllowed(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws Exception {
-//        Subject subject = getSubject(servletRequest, servletResponse);
-//        return null != subject.getPrincipals();
-//    }
-//
-//    @Override
-//    protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws IOException {
-//        if(isAjax(((HttpServletRequest) servletRequest))) {
-//            WebUtils.toHttp(servletResponse).sendError(401);
-//        } else {
-//            String unauthorizedUrl = getUnauthorizedUrl();
-//            if(StringUtils.hasText(unauthorizedUrl)) {
-//                WebUtils.issueRedirect(servletRequest, servletResponse, unauthorizedUrl);
-//            } else {
-//                WebUtils.toHttp(servletResponse).sendError(401);
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    private boolean isAjax(HttpServletRequest request) {
-//        String header = request.getHeader("x-requestd-with");
-//        return null != header && "XMLHttpRequest".endsWith(header);
-//    }
-//
-//    @Override
-//    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-//        Subject subject = SecurityUtils.getSubject();
-//        if (isAjax(httpServletRequest)) {
-//            if (org.apache.commons.lang3.StringUtils.contains(httpServletRequest.getRequestURI(), "/portal/admin/login")) {
-//                return true;
-//            }
-//            if (subject.isAuthenticated()) {
-//                return true;
-//            } else {
-//                MyDTO myDTO = new MyDTO();
-//                httpServletResponse.setCharacterEncoding("UTF-8");
-//                httpServletResponse.setContentType("application/json");
-//                myDTO.setCode(401);
-//                myDTO.setMsg("noAuthorization");
-//                httpServletResponse.getWriter().write(myDTO.toString());
-//                return false;
-//            }
-//        } else {
-//            //不是ajax进行重定向处理
-////          httpServletResponse.sendRedirect("/login/local");
-//            return true;
-//        }
-//    }
 
 
     /**
-     * 判断是否登录。 在登录的情况下会执行这个方法，此方法返回true直接访问控制器
+     * 判断是否登录。 在登录的情况下会执行这个方法，此方法返回true直接访问控制器. 如果isAccessAllowed返回false 则执行onAccessDenied
      * @param request request
      * @param response response
      * @param mappedValue mappedValue
      * @return boolean
      */
     @Override
-    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue){
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        if (request instanceof HttpServletRequest) {
+            if ("OPTIONS".equals(((HttpServletRequest) request).getMethod().toUpperCase())) {
+                return true;
+            }
+        }
+        return super.isAccessAllowed(request, response, mappedValue);
+    }
+
+
+    /**
+     * 在访问controller前判断是否登录，返回json，不进行重定向。
+     * 是否拒绝登录。 在没有登录的情况下访问此方法。
+     * @param request request
+     * @param response response
+     * @return boolean
+     * @throws IOException IOException
+     */
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws IOException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", ((HttpServletRequest) request).getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        MyDTO myDTO = new MyDTO(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", null);
+        httpServletResponse.getWriter().write(JSONObject.toJSON(myDTO).toString());
         return false;
     }
-
-    @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-        //加了一次过滤
-        if (isAjax(request)) {
-            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-            httpServletResponse.setCharacterEncoding("UTF-8");
-            httpServletResponse.setContentType("application/json");
-            httpServletResponse.setStatus(401);
-            return false;
-        }
-        return super.onAccessDenied(request, response);
-    }
-
-    private boolean isAjax(ServletRequest request) {
-        String header = ((HttpServletRequest) request).getHeader("X-Requested-With");
-        if ("XMLHttpRequest".equalsIgnoreCase(header)) {
-            return Boolean.TRUE;
-        }
-        return Boolean.FALSE;
-    }
-
-
-//    /**
-//     * 是否拒绝登录。 在没有登录的情况下访问此方法。
-//     * @param request request
-//     * @param response response
-//     * @return boolean
-//     * @throws Exception boolean
-//     */
-//    @Override
-//    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-//        if (isLoginRequest(request, response)) {
-//            if (!isLoginSubmission(request, response)) {
-//                HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-//                httpServletResponse.setContentType("application/json;charset=UTF-8");
-//                httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-//                JSONObject jsonObject = new JSONObject();
-//                jsonObject.put("message", "没有权限访问...");
-//                jsonObject.put("code", "401");
-//                jsonObject.put("data", "");
-//                Writer writer = httpServletResponse.getWriter();
-//                writer.write(jsonObject.toJSONString());
-//                writer.flush();
-//                writer.close();
-//            }else {
-//                return executeLogin(request, response);
-//            }
-//        }
-//        return false;
-//    }
-
 }
