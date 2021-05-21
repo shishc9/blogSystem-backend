15eac77fb2117fcca6ad8d767ff5dea786b56374
shishc9 <343141457@qq.com>
2021-05-21 18:06:07 +0800

    update perms init.sql

--- src/main/resources/sql/blog-backend-init.sql
+++ src/main/resources/sql/blog-backend-init.sql
@@ -71,14 +71,28 @@ INSERT INTO perms(user_identity, entity, perm)
 INSERT INTO perms(user_identity, entity, perm)
     VALUES(0, 'BLOG', 'INSERT');
 INSERT INTO perms(user_identity, entity, perm)
-    VALUES(1, 'BLOG', 'DELETE');
-
-
-INSERT INTO perms(user_identity, entity, perm)
     VALUES(0, 'USER', 'UPDATE');
 INSERT INTO perms(user_identity, entity, perm)
     VALUES(0, 'USER', 'DELETE');
 INSERT INTO perms(user_identity, entity, perm)
+    VALUES(0, 'COMMENT', 'DELETE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(0, 'COMMENT', 'CREATE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(0, 'LIKE', 'CREATE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(0, 'LIKE', 'DELETE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(0, 'CHANGE', 'UPDATE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(1, 'BLOG', 'DELETE');
+INSERT INTO perms(user_identity, entity, perm)
     VALUES(1, 'USER', 'UPDATE');
 INSERT INTO perms(user_identity, entity, perm)
     VALUES(1, 'USER', 'DELETE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(1, 'COMMENT', 'DELETE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(1, 'CHANGE', 'UPDATE');
+INSERT INTO perms(user_identity, entity, perm)
+    VALUES(1, 'MANAGE', 'READ');
