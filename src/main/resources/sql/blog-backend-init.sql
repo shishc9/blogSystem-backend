39a7916edadfa2cf93200069deecce3d8898b2b7
shishc9 <343141457@qq.com>
2021-05-22 10:05:03 +0800

    fix : [email & sql]
    fix email login.
    fix init.sql

--- src/main/resources/sql/blog-backend-init.sql
+++ src/main/resources/sql/blog-backend-init.sql
@@ -1,31 +1,28 @@
 use blogsys;
 
+# USER INIT
 INSERT INTO user(username, password, user_identity, age, gender, email)
-    VALUES('blogger1', '123456', 0, 20, 'MALE',  '123456789@126.com');
+    VALUES('blogger', '02571c02bcabe4bfc42be1306ae9ff9c', 0, 20, 'MALE',  '123456789@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('A', '111111', 20, '111111111@126.com');
+    VALUES('A', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '111111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('B', '111111', 20, '121111111@126.com');
+    VALUES('B', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '121111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('C', '111111', 20, '131111111@126.com');
+    VALUES('C', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '131111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('D', '111111', 20, '141111111@126.com');
+    VALUES('D', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '141111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('E', '111111', 20, '151111111@126.com');
+    VALUES('E', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '151111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('F', '111111', 20, '161111111@126.com');
+    VALUES('F', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '161111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('G', '111111', 20, '171111111@126.com');
+    VALUES('G', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '171111111@126.com');
 INSERT INTO user(username, password, age, email)
-    VALUES('H', '111111', 20, '181111111@126.com');
+    VALUES('H', '02571c02bcabe4bfc42be1306ae9ff9c', 20, '181111111@126.com');
 INSERT INTO user(username, password, user_identity, age, gender, email)
-    VALUES('blogger2', '123456', 0, 20, 'MALE',  '123456780@126.com');
-INSERT INTO user(username, password, user_identity, age, gender, email)
-    VALUES('admin2', '123456', 1, 20, 'MALE',  'shishc9@126.com');
+    VALUES('admin', 'db18a83510dbb5a3d38e5cec4ab1b047', 1, 20, 'MALE',  'shishc9@126.com');
 
-
-
-
+# BLOG INIT
 INSERT INTO blog(user_id, title, content) values (1, '第一篇博客', '应该能正常发出去吧');
 INSERT INTO blog(user_id, title, content) values (1, 'Java基础', '应该能正常发出去吧');
 INSERT INTO blog(user_id, title, content) values (1, 'Java进阶', '应该能正常发出去吧');
@@ -41,8 +38,7 @@ INSERT INTO blog(user_id, title, content) values (10, 'admin24', '应该能正�
 INSERT INTO blog(user_id, title, content) values (10, 'admin25', '应该能正常发出去吧');
 INSERT INTO blog(user_id, title, content) values (10, 'admin26', '应该能正常发出去吧');
 
-
-
+# COMMENT INIT
 INSERT INTO comment(blog_id, username, content, parent_comment_id)
     VALUES('30000', 'D', 'first comment', 0);
 INSERT INTO comment(blog_id, username, content, parent_comment_id)
@@ -56,6 +52,7 @@ INSERT INTO comment(blog_id, username, content, parent_comment_id)
 INSERT INTO comment(blog_id, username, content, parent_comment_id)
     VALUES('30000', 'A', 'reply D', 50004);
 
+#MESSAGE INIT
 INSERT INTO message(user_id, username, content, gmt_create, parent_comment_id)
     VALUES(3, 'B', 'leave message1', NOW(), 0);
 INSERT INTO message(user_id, username, content, gmt_create, parent_comment_id)
@@ -63,7 +60,7 @@ INSERT INTO message(user_id, username, content, gmt_create, parent_comment_id)
 INSERT INTO message(user_id, username, content, gmt_create, parent_comment_id)
     VALUES(3, 'C', 'reply B leave message', NOW(), 50002);
 
-
+# PERMS INIT
 INSERT INTO perms(user_identity, entity, perm)
     VALUES(0, 'BLOG', 'UPDATE');
 INSERT INTO perms(user_identity, entity, perm)
